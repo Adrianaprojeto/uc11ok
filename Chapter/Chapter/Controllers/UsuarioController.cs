2a78ec53f5e73004b6393d60be0cb90792ba629a
Adrianaok <adrianabeco203@gmail.com>
2023-05-02 00:16:25 -0300

    segundo comit

---
+++ Chapter/Chapter/Controllers/UsuarioController.cs
@@ -0,0 +1,92 @@
+﻿using Chapter.Interfaces;
+using Chapter.Models;
+using Chapter.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using System.Linq.Expressions;
+
+namespace Chapter.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepository _iUsuarioRepository;
+
+        public UsuarioController(IUsuarioRepository usuarioRepository)
+        {
+            _iUsuarioRepository = usuarioRepository;
+        }
+
+        [HttpGet]
+
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_iUsuarioRepository.Listar());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+
+        [HttpGet("{id}")]
+
+        public IActionResult BuscarPorId(int id)
+        {
+
+            try
+            {
+                Usuario usuarioEncontrado = _iUsuarioRepository.BuscarPorId(id);
+
+                if (usuarioEncontrado == null)
+                {
+                    return NotFound();
+                }
+                return Ok(usuarioEncontrado);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [HttpPost("{id}")]
+
+        public IActionResult Alterar(int id, Usuario usuario) 
+        {
+         try
+            {
+                _iUsuarioRepository.Atualizar(id, usuario);
+                return Ok("Usuario alterado");    
+            }
+               catch (Exception)
+            {
+                throw;
+            }
+        }
+        [HttpDelete("{id}")]
+
+        public IActionResult Deletar(int id) 
+        {
+         try
+            {
+                _iUsuarioRepository.Deletar(id);
+                return Ok("Usuario Deletado");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }    
+    
+
+}
---
+++ Chapter/Chapter/Repositories/UsuarioRepository.cs
@@ -0,0 +1,98 @@
+﻿using Chapter.Contexts;
+using Chapter.Interfaces;
+using Chapter.Models;
+
+
+namespace Chapter.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+
+    {
+        private readonly ChapterContext _context;
+
+        public UsuarioRepository(ChapterContext context)
+        {
+            _context = context;
+        }
+
+        public void Atualizar(int id, Usuario usuario)
+        {
+            Usuario usuarioEncontrado = _context.Usuarios.Find(id);
+
+            if (usuarioEncontrado != null)
+            {
+                usuarioEncontrado.Email = usuario.Email;
+                usuarioEncontrado.Senha = usuario.Senha;
+                usuarioEncontrado.Tipo = usuario.Tipo;
+
+                _context.Usuarios.Update(usuarioEncontrado);
+
+                _context.SaveChanges();
+            }
+
+        }
+        public Usuario BuscarPorId(int id)
+        {
+            return _context.Usuarios.Find(id);
+        }
+
+        public void Cadastrar(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+
+            _context.SaveChanges();
+
+        }
+        public void Deletar(int id)
+        {
+            Usuario usuarioEncontrado = _context.Usuarios.Find(id);
+
+            _context.Usuarios.Remove(usuarioEncontrado);
+
+            _context.SaveChanges();
+
+        }
+        public List<Usuario> Listar()
+
+        {
+          return _context.Usuarios.ToList();
+        }
+
+        public Usuario login(string email, string senha)
+        {
+
+            return _context.Usuarios.FirstOrDefault(x => x.Email == email && x.Senha == senha);
+        }
+
+        void IUsuarioRepository.Atualizar(int id, Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        Usuario IUsuarioRepository.BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IUsuarioRepository.Cadastrar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IUsuarioRepository.Deletar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Usuario> IUsuarioRepository.Listar()
+        {
+            throw new NotImplementedException();
+        }
+
+        Usuario IUsuarioRepository.Login(string email, string senha)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
